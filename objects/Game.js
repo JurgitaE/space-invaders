c2e7c7e365a478a4293d9280ad1661405341abe3
Jurgita <jurgita.eicinaite@yahoo.com>
2023-11-03 16:39:45 +0200

    left and right controls

--- objects/Game.js
+++ objects/Game.js
@@ -5,7 +5,16 @@ class Game {
         this.canvas = canvas;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
+        this.keys = [];
         this.player = new Player(this);
+
+        window.addEventListener('keydown', e => {
+            !this.keys.includes(e.key) && this.keys.push(e.key);
+        });
+        window.addEventListener('keyup', e => {
+            const index = this.keys.indexOf(e.key);
+            index > -1 && this.keys.splice(index, 1);
+        });
     }
 
     render(context) {
--- objects/Player.js
+++ objects/Player.js
@@ -5,13 +5,16 @@ class Player {
         this.height = 100;
         this.x = this.game.width * 0.5 - this.width * 0.5;
         this.y = this.game.height - this.height;
-        this.speed = -1;
+        this.speed = 10;
     }
     draw(context) {
         context.fillRect(this.x, this.y, this.width, this.height);
     }
     update() {
-        this.x += this.speed;
+        if (this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
+        if (this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
+        if (this.x < 0) this.x = 0;
+        else if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
     }
 }
 export default Player;
