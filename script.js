6214b79e5fc1abab8ddc4468691b3a533f45cc0e
Jurgita <jurgita.eicinaite@yahoo.com>
2023-11-03 15:56:24 +0200

    initial structure and classes

---
+++ script.js
@@ -0,0 +1,17 @@
+import Game from './objects/Game.js';
+
+window.addEventListener('load', function () {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 600;
+    canvas.height = 800;
+    const game = new Game(canvas);
+    game.render(ctx);
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.render(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
+});
